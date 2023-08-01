d489e96285e1600ffcf12979ea7433474ceacc7d
sargon64 <niko@velvet.moe>
2023-07-31 19:58:49 -0700

    feat: support dowloading dll

--- src/cdn.rs
+++ src/cdn.rs
@@ -1,12 +1,24 @@
 use actix_web::{get, post, web, Error, HttpResponse, Responder};
 use entity::prelude::*;
+use forge_lib::structs::forgemod::ForgeMod;
 use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+use serde::Deserialize;
 
 use crate::Database;
 
-#[get("/cdn/{slug}@{version}")]
-async fn cdn_get(db: web::Data<Database>, path: web::Path<(String, String)>) -> impl Responder {
-    let (slug, version) = path.into_inner();
+#[derive(Copy, Clone, Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum CdnType {
+    Dll,
+    Package,
+}
+
+async fn cdn_handler(
+    db: web::Data<Database>,
+    slug: String,
+    version: String,
+    dl_type: CdnType,
+) -> impl Responder {
     let db_mod = Mods::find()
         .filter(entity::mods::Column::Slug.eq(&slug))
         .one(&db.pool)
@@ -29,16 +41,50 @@ async fn cdn_get(db: web::Data<Database>, path: web::Path<(String, String)>) -> 
                 Ok(file) => file,
                 Err(_) => return HttpResponse::NotFound().finish(),
             };
+            match dl_type {
+                CdnType::Dll => {
+                    let dll = ForgeMod::try_from(&*file).unwrap().artifact_data;
 
-            return HttpResponse::Ok()
-                .content_type("application/octet-stream")
-                .append_header((
-                    "Content-Disposition",
-                    format!("attachment; filename=\"{}-v{}.beatforge\"", slug, version),
-                ))
-                .body(file);
+                    return HttpResponse::Ok()
+                        .content_type("application/octet-stream")
+                        .append_header((
+                            "Content-Disposition",
+                            format!("attachment; filename=\"{}-v{}.dll\"", slug, version),
+                        ))
+                        .body(dll);
+                }
+                CdnType::Package => {
+                    return HttpResponse::Ok()
+                        .content_type("application/octet-stream")
+                        .append_header((
+                            "Content-Disposition",
+                            format!("attachment; filename=\"{}-v{}.beatforge\"", slug, version),
+                        ))
+                        .body(file);
+                }
+            }
         }
     }
 
     HttpResponse::NotFound().finish()
+}
+
+#[get("/cdn/{slug}@{version}/{type}")]
+async fn cdn_get(
+    db: web::Data<Database>,
+    path: web::Path<(String, String, CdnType)>,
+) -> impl Responder {
+    let (slug, version, dl_type) = path.into_inner();
+
+    cdn_handler(db, slug, version, dl_type).await
+}
+
+#[get("/cdn/{slug}@{version}")]
+async fn cdn_get_typeless(
+    db: web::Data<Database>,
+    path: web::Path<(String, String)>,
+) -> impl Responder {
+    let (slug, version) = path.into_inner();
+    
+    cdn_handler(db, slug, version, CdnType::Package).await
 }
